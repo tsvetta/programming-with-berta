49586a9dbd3b5168bc3915ac97aab92fb26c06ca
Tatyna Tsvetkova <tsvetta@gmail.com>
2018-04-09 00:35:11 +0300

    more phrases

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -5,20 +5,47 @@ import BertaSays from 'Components/berta-says';
 
 import styles from './styles.css';
 
+const DIALOG = [
+  {
+    Berta: "Hello, good sir! I'm Berta the Dog!",
+    you: "What's up, Berta?",
+  },
+  {
+    Berta: "I'm having troubles...",
+    you: "What troubles, Berta?",
+  },
+  {
+    Berta: "...troubles with programming!",
+    you: "Tell me more!",
+  },
+  {
+    Berta: "Later.",
+    you: "Ok :(",
+  },
+];
+
 class HomePage extends React.PureComponent {
+  state = {
+    counter: 0,
+  }
+
   render() {
     return (
       <Page>
         <div className={styles.content}>
-          <BertaSays
-            phrase="Hi! I'm Berta the Dog!"
-          />
-          <button type='button' className={styles.dialogButton}>
-            What's up Berta?
+          <BertaSays phrase={DIALOG[this.state.counter].Berta} />
+          <button type='button' className={styles.dialogButton} onClick={this.nextPhrase}>
+            {DIALOG[this.state.counter].you}
           </button>
         </div>
       </Page>
     );
+  }
+
+  nextPhrase = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
   }
 }
 
