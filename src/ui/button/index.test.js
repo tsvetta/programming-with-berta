e902a13fe38d997a8e81c871c67bb6f7672db8ea
Tatyna Tsvetkova <tsvetta@gmail.com>
2018-04-21 20:40:43 +0300

    build

--- src/ui/button/index.test.js
+++ src/ui/button/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
-import Button from './';
+import Button from 'UI/button';
 
 describe('Button', () => {
   it('should render', () => {
